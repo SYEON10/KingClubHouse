c2d4ebb37f9dfa8d82e776625ee7d050e043228d
SYEON10 <131231117+SYEON10@users.noreply.github.com>
2025-06-12 01:14:59 +0000

    Deploying to gh-pages from @ SYEON10/KingClubHouse@736e2b22d53557377df435be8c2ef315b74cc4e2 🚀

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
-const cacheName = "DefaultCompany-KINGRoom-0.0.5";
+const cacheName = "DefaultCompany-KINGRoom-0.0.6";
 const contentToCache = [
     "Build/WebGL.loader.js",
-    "Build/WebGL.framework.js",
-    "Build/WebGL.data",
-    "Build/WebGL.wasm",
+    "Build/cccb55245bdf70095b0aa2fbad857a15.js",
+    "Build/3e72d85c2d4765ce3b3bda2024e124c9.data",
+    "Build/f59463d42553f4456088fea84ee1ddfa.wasm",
     "TemplateData/style.css"
 
 ];
